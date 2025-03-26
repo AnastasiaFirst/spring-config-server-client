e7d6b625aed83904b34da97229ef482a7de40abc
AnastasiaFirst <92177620+AnastasiaFirst@users.noreply.github.com>
2025-03-26 18:00:36 +0300

    Update ClientconfigApplication.java

--- SpringCloudConfigServer/clientconfig/src/main/java/com/example/ClientconfigApplication.java
+++ SpringCloudConfigServer/clientconfig/src/main/java/com/example/ClientconfigApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UserProperties.class)
 public class ClientconfigApplication {
 
     public static void main(String[] args) {
